c230c7addf50bcce9825a535770f3445d0862686
Marcos <marcosraimondi1@gmail.com>
2021-01-24 20:22:57 -0300

    firestore db

--- Components/auth/Register.js
+++ Components/auth/Register.js
@@ -20,11 +20,19 @@ export class Register extends Component {
         // alert("PRESSED!")
         const { email, name, password } = this.state
         firebase.auth().createUserWithEmailAndPassword(email, password) //returns a promise, async function
-            // handle promis with .then .catch
+            // handle promise with .then .catch
             .then((result) => {
+                alert("WORKING")
+                firebase.firestore().collection("users")
+                    .doc(firebase.auth().currentUser.uid)
+                    .set({
+                        name,
+                        email
+                    })
                 console.log(result)
             })
             .catch((error) => {
+                alert("ERROR: ", JSON.stringify(error.message))
                 console.log('error: ', error)
             })
     }
