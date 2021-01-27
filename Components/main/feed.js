a2093d83f53b479006913bd6340ab0272102b613
Marcos <marcosraimondi1@gmail.com>
2021-01-27 16:42:33 -0300

    Feed Screen

--- Components/main/feed.js
+++ Components/main/feed.js
@@ -1,16 +1,84 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Image, FlatList, Button } from 'react-native'
+import { connect } from 'react-redux'
+import firebase from 'firebase'
+require('firebase/firestore')
 
-export class feed extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-                <Text>
-                    FEED
-                </Text>
+function Feed(props) {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        let posts = [];
+        if (props.usersLoaded == props.following.length) {
+            for (let i = 0; i < props.following.length; i++) {
+                const user = props.users.find(el => el.uid === props.following[i])
+                if (user != undefined) {
+                    posts = [...posts, ...user.posts]
+                }
+            }
+
+            posts.sort(function (x, y) {
+                return x.creation - y.creation
+            })
+
+            setPosts(posts)
+        }
+    }, [props.usersLoaded])
+
+    console.log("POSTS: ", posts)
+    console.log("USERS: ", props.users)
+    console.log("FOLLOWING: ", props.following)
+    console.log("USERS LOADED: ", props.usersLoaded)
+    return (
+        <View style={styles.container}>
+            <View style={styles.containerGallery}>
+                <FlatList
+                    numColumns={1}
+                    horizontal={false}
+                    data={posts}
+                    renderItem={({ item }) => (
+                        <View style={styles.containerImage}>
+                            <Text style={styles.container}>{item.user.name}</Text>
+                            <Image
+                                style={styles.image}
+                                source={{ uri: item.downloadURL }}
+                            />
+                        </View>
+                    )}
+                />
             </View>
-        )
-    }
+        </View>
+    )
 }
 
-export default feed
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 40
+    },
+    containerInfo: {
+        margin: 20
+    },
+    containerGallery: {
+        flex: 1,
+        margin: 20
+    },
+    containerImage: {
+        flex: 1 / 3
+    },
+    image: {
+        flex: 1,
+        aspectRatio: 1 / 1
+    }
+})
+
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    following: store.userState.following,
+
+    users: store.usersState.users,
+    usersLoaded: store.usersState.usersLoaded
+})
+
+export default connect(mapStateToProps, null)(Feed)
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
-import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from '../constants/index'
+import { USERS_POSTS_STATE_CHANGE, USERS_DATA_STATE_CHANGE, USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from '../constants/index'
 
 export function fetchUser() {
     return ((dispatch) => {
@@ -51,6 +51,57 @@ export function fetchUserFollowing() {
                     return id
                 })
                 dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following })
+                for (let i = 0; i < following.length; i++) {
+                    dispatch(fetchUsersData(following[i]));
+                }
+            })
+    })
+}
+
+export function fetchUsersData(uid) {
+    return ((dispatch, getState) => {
+        // see if there is element already in users
+        const found = getState().usersState.users.some(el => el.uid === uid)
+        if (!found) {
+            firebase.firestore()
+                .collection("users")
+                .doc(uid)
+                .get()
+                .then((snapshot) => {
+                    if (snapshot.exists) {
+                        let user = snapshot.data()
+                        user.uid = snapshot.id
+                        dispatch({ type: USERS_DATA_STATE_CHANGE, user })
+                        dispatch(fetchUsersFollowingPosts(user.uid))
+                    }
+                    else {
+                        console.log('does not exist')
+                    }
+                })
+        }
+    })
+}
+
+export function fetchUsersFollowingPosts(uid) {
+    return ((dispatch, getState) => {
+        firebase.firestore()
+            .collection("posts")
+            .doc(uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+                const uid = snapshot.query.EP.path.segments[1]; // == uid
+                // uid -> its async so first var [uid] may change during this function execution
+                //console.log(uid)
+                const user = getState().usersState.users.find(el => el.uid === uid)
+
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data()
+                    const id = doc.id
+                    return { id, ...data, user }
+                })
+                dispatch({ type: USERS_POSTS_STATE_CHANGE, posts, uid })
             })
     })
 }
